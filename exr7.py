d3738c48eda5ebcf1e97fbba6ea9cc0c37c54c6f
jhonataconstantinobasilio <jhonataconstantinobasilio@gmail.com>
2025-06-18 10:14:50 -0300

    Add files via upload

---
+++ exr7.py
@@ -0,0 +1,6 @@
+letra =input("digite sua vogal")
+
+if letra in 'aeiou':
+   print("é vogal")
+else:
+     print("consoante")
---
+++ exr9.py
@@ -0,0 +1,25 @@
+semana = int(input("digite seu numero"))
+
+if semana ==1:
+    print('domingo')
+
+elif semana ==2:
+    print('segunda')
+
+elif semana ==3:
+    print('terça')
+
+elif semana ==4:
+    print('quarta')
+
+elif semana ==5:
+     print('quinta')
+
+elif semana ==6:
+    print('sexta')
+
+elif semana ==7:
+    print('sabado')
+
+else:
+    print('não é possivel encontrar esse numero por favor tente outro numero')    
