0c5aab760acb9ae4e5415796a83dcf921be2b767
jhonataconstantinobasilio <jhonataconstantinobasilio@gmail.com>
2025-07-02 08:32:03 -0300

    Add files via upload

---
+++ exr1atv4comfor.py
@@ -0,0 +1,6 @@
+lista =[1,2,3,4,5,6,7,8,9,10]
+
+tabuada= int(input("digite seu numero\n"))
+
+for item in lista:
+    print(tabuada*item)
---
+++ exr2atv4comfor.py
@@ -0,0 +1,8 @@
+lista= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for item in lista:
+    
+   if item %2==0:
+      print('é par')
+   else:
+       print('é impar')   
