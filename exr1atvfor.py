0dab3b49590b579f2e00bcb5e2db208798ada270
jhonataconstantinobasilio <jhonataconstantinobasilio@gmail.com>
2025-07-30 10:08:57 -0300

    Add files via upload

---
+++ exr1atvfor.py
@@ -0,0 +1,4 @@
+lista = ["4,5,6,7,8"]
+
+for item in lista:
+    print(item+'numero')
---
+++ exr2atvfor.py
@@ -0,0 +1,10 @@
+lista = ["Paulo","Matheus","Jorge","Maria"]
+
+for item in lista:
+    
+
+    if item == 'Maria':
+        print(item,"está devendo pro tigrinho")
+
+    else:
+        print(item,"não está devendo pro tigrinho")   
